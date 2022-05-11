c223d846b61793fabfa849f4d1ffebeb46b677dd
Handkep <paulus.handke@gmail.com>
2022-05-11 21:35:57 +0200

    dev

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 HiotDevice device;
 
 void setup() {
-	device.setLEDPins(13,15,12); //v4
-	// device.setLEDPins(14,12,13); // pcb
+	// device.setLEDPins(13,15,12); //v4
+	device.setLEDPins(14,12,13); // pcb
 	// device.setLEDPinsRGBW(14,12,13,15);
 	// device.setLEDPinsRGBW(13,15,12,14); // fenstertreppe
 	device.setup();
